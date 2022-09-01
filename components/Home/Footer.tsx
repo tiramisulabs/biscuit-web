119c0130529167ea2c08bddeb81a605cf1018d56
kaname-png <inmortaldragonxspace@outlook.com>
2022-09-01 11:39:31 -0600

    feat(pages/home): added skeleton loader to chips with stats in footer

--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Chip from '@mui/material/Chip';
+import Skeleton from '@mui/material/Skeleton';
 
 import GitHubIcon from '@mui/icons-material/GitHub';
 import DownloadIcon from '@mui/icons-material/Download';
@@ -11,6 +12,7 @@ import StarsIcon from '@mui/icons-material/Stars';
 import KeyboardCommandKeyIcon from '@mui/icons-material/KeyboardCommandKey';
 import CheckIcon from '@mui/icons-material/Check';
 
+import { Else, If, Then } from 'react-if';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import { wait } from '../../lib/funtions/utils';
@@ -49,16 +51,30 @@ const HomeFooter = () => {
 				</CopyToClipboard>
 			</Stack>
 			<Stack spacing={2} direction="row" marginTop={3} justifyContent={{ xs: 'center', sm: 'unset' }}>
-				<Chip
-					label={stats.loading ? '... downloads' : `${stats.stats.npm.downloads.toLocaleString()} downloads`}
-					color="secondary"
-					icon={<DownloadIcon></DownloadIcon>}
-				/>
-				<Chip
-					label={stats.loading ? '... Stars on Github' : `${stats.stats.repository.stargazers.toLocaleString()} Stars on Github`}
-					color="warning"
-					icon={<StarsIcon></StarsIcon>}
-				/>
+				<If condition={stats.loading}>
+					<Then>
+						<Skeleton animation="wave" width={190} height={50} />
+					</Then>
+					<Else>
+						<Chip
+							label={stats.loading ? '... downloads' : `${stats.stats.npm.downloads.toLocaleString()} downloads`}
+							color="secondary"
+							icon={<DownloadIcon></DownloadIcon>}
+						/>
+					</Else>
+				</If>
+				<If condition={stats.loading}>
+					<Then>
+						<Skeleton animation="wave" width={190} height={50} />
+					</Then>
+					<Else>
+						<Chip
+							label={stats.loading ? '... Stars on Github' : `${stats.stats.repository.stargazers.toLocaleString()} Stars on Github`}
+							color="warning"
+							icon={<StarsIcon></StarsIcon>}
+						/>
+					</Else>
+				</If>
 			</Stack>
 		</div>
 	);
