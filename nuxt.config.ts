34e15bca1acba01081ab891f712170129c13cbc8
kaname-png <56084970+kaname-png@users.noreply.github.com>
2023-01-12 10:29:09 -0600

    chore: tools for perfomance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/image-edge"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/image-edge",
+    "@nuxtjs/web-vitals",
+    "@nuxtjs/fontaine",
+  ],
   app: {
     head: {
       titleTemplate:
