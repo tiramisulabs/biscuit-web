a1b78ed6c7fb3e7d97e5c7bdc7c34e27e2c2d9e6
kaname-png <56084970+kaname-png@users.noreply.github.com>
2023-01-14 16:27:34 -0600

    perf: some ui tweaks

--- components/AppToast.vue
+++ components/AppToast.vue
@@ -1,6 +1,13 @@
 <template>
 	<Teleport to="body">
-		<Transition>
+		<Transition
+			enter-active-class="transition ease-out duration-100"
+			enter-from-class="transform opacity-0 scale-95"
+			enter-to-class="transform opacity-100 scale-100"
+			leave-active-class="transition ease-in duration-75"
+			leave-from-class="transform opacity-100 scale-100"
+			leave-to-class="transform opacity-0 scale-95"
+		>
 			<div v-if="visible" class="p-3 lg:p-0 absolute z-50 botton-10 lg:top-10 lg:right-10 lg:w-72 w-full">
 				<div
 					class="flex w-full p-4 bg-gray-800 rounded-lg items-center space-x-2"
