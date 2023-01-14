61ed1d4465a63f16a0545d4bdfebbcfb53db2661
kaname-png <56084970+kaname-png@users.noreply.github.com>
2023-01-13 18:16:03 -0600

    chore: some ui tweaks

--- components/AppToast.vue
+++ components/AppToast.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="p-3 lg:p-0 absolute z-50 botton-10 lg:top-10 lg:right-10 lg:w-72 w-full">
 		<div class="flex w-full p-4 bg-gray-800 rounded-lg items-center justify-between space-x-2" :class="[color]">
-			<div class="rounded-full p-2 bg-gray-300/20 shrink-0">
+			<div class="rounded-full p-2 bg-gray-300/20 shrink-0 w-8 h-8 items-center flex">
 				<Icon :name="iconLeft" class="text-lg lg:text-xl" />
 			</div>
 			<p class="shrink-0">{{ message }}</p>
 			<button
 				v-if="closeButton"
-				class="rounded-full p-2 bg-gray-300/20 hover:bg-gray-300/50 active:bg-gray-300/50 shrink-0"
+				class="rounded-full p-2 bg-gray-300/20 hover:bg-red-300/50 active:bg-red-300/50 shrink-0 w-8 h-8 items-center flex"
 				@click="closeButton?.()"
 			>
 				<Icon name="lucide:x" class="text-lg lg:text-xl" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<main class="mx-auto px-6 lg:px-12 max-w-screen-xl flex justify-between flex-col space-y-44">
-		<section class="flex py-10 lg:py-32 space-y-10 lg:space-y-0 lg:space-x-10 flex-col lg:flex-row">
+		<section class="flex py-10 lg:py-28 space-y-10 lg:space-y-0 lg:space-x-10 flex-col lg:flex-row">
 			<div class="w-full lg:w-1/2 space-y-8">
 				<div class="flex flex-col space-y-5">
 					<div class="flex text-gray-500 dark:text-gray-300 items-center space-x-2">
@@ -23,39 +23,39 @@
 				</div>
 				<ul class="text-black dark:text-white font-medium tracking-wider">
 					<li class="space-x-2 my-3 items-center flex">
-						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-fit">
+						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-10 h-10 items-center flex">
 							<Icon name="lucide:framer" class="text-lg lg:text-xl text-yellow-400" />
 						</div>
 						<span class="text-sm lg:text-md">Memory efficient, -100Mb on 200k servers.</span>
 					</li>
 					<li class="space-x-2 my-3 items-center flex">
-						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-fit">
+						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-10 h-10 items-center flex">
 							<Icon name="lucide:clipboard-signature" class="text-lg lg:text-xl text-green-400" />
 						</div>
 						<span class="text-sm lg:text-md">Easy to use, utility classes for better commands.</span>
 					</li>
 					<li class="space-x-2 my-3 items-center flex">
-						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-fit">
+						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-10 h-10 items-center flex">
 							<Icon name="lucide:network" class="text-lg lg:text-xl text-purple-400" />
 						</div>
 						<span class="text-sm lg:text-md">Escable, with microservices.</span>
 					</li>
 					<li class="space-x-2 my-3 items-center flex">
-						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-fit">
+						<div class="rounded-full p-2 bg-gray-300/10 dark:bg-gray-200/10 w-10 h-10 items-center flex">
 							<Icon name="lucide:package-check" class="text-lg lg:text-xl text-red-400" />
 						</div>
 						<span class="text-sm lg:text-md">Optional cache, no more mandatory cache.</span>
 					</li>
 				</ul>
 				<div class="flex space-y-4 md:space-y-0 space-x-0 md:space-x-4 items-center flex-col md:flex-row">
-					<SimpleButton
-						class="w-full lg:w-fit"
+					<div
+						class="w-full lg:w-fit bg-black p-3 rounded-lg font-mono text-white shadow-black/30 dark:shadow-black/70 shadow-xl cursor-pointer"
 						button-style="solid"
 						icon-left="lucide:party-popper"
 						@click="copyInstallCommand()"
 					>
-						Get started
-					</SimpleButton>
+						$ npm install @biscuitland/core
+					</div>
 					<NuxtLink class="w-full lg:w-fit" :href="config.public.githubUrl" target="_blank">
 						<SimpleButton class="w-full lg:w-fit" button-style="ghost" icon-left="lucide:github">
 							Github
@@ -127,37 +127,37 @@
 				<div
 					class="w-full p-4 rounded-xl bg-green-400 hover:ring-4 transition duration-300 hover:ring-green-600 dark:hover:ring-green-800 hover:-translate-y-2 space-y-4"
 				>
-					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg">
+					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg text-white">
 						<Icon class="text-3xl" name="lucide:download" />
 					</div>
-					<h1 class="text-4xl lg:text-5xl font-bold">{{ stats?.downloads }}+</h1>
+					<h1 class="text-4xl lg:text-5xl font-bold text-white">{{ stats?.downloads }}+</h1>
 					<h5 class="text-lg lg:text-xl text-white font-medium">— Downloads</h5>
 				</div>
 				<div
 					class="w-full p-4 rounded-xl bg-orange-400 hover:ring-4 transition duration-300 hover:ring-orange-600 dark:hover:ring-orange-800 hover:-translate-y-2 space-y-4"
 				>
-					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg">
+					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg text-white">
 						<Icon class="text-3xl" name="lucide:star" />
 					</div>
-					<h1 class="text-4xl lg:text-5xl font-bold">{{ stats?.stargazers }}+</h1>
+					<h1 class="text-4xl lg:text-5xl font-bold text-white">{{ stats?.stargazers }}+</h1>
 					<h5 class="text-lg lg:text-xl text-white font-medium">— Stars</h5>
 				</div>
 				<div
 					class="w-full p-4 rounded-xl bg-purple-400 hover:ring-4 transition duration-300 hover:ring-purple-600 dark:hover:ring-purple-800 hover:-translate-y-2 space-y-4"
 				>
-					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg">
+					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg text-white">
 						<Icon class="text-3xl" name="lucide:github" />
 					</div>
-					<h1 class="text-4xl lg:text-5xl font-bold">{{ stats?.forks }}+</h1>
+					<h1 class="text-4xl lg:text-5xl font-bold text-white">{{ stats?.forks }}+</h1>
 					<h5 class="text-lg lg:text-xl text-white font-medium">— Forks</h5>
 				</div>
 				<div
 					class="w-full p-4 rounded-xl bg-blue-400 hover:ring-4 transition duration-300 hover:ring-blue-600 dark:hover:ring-blue-800 hover:-translate-y-2 space-y-4"
 				>
-					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg">
+					<div class="bg-gray-200/30 w-fit p-1 lg:p-2 rounded-lg text-white">
 						<Icon class="text-3xl" name="lucide:users" />
 					</div>
-					<h1 class="text-4xl lg:text-5xl font-bold">{{ stats?.discord_support_members }}+</h1>
+					<h1 class="text-4xl lg:text-5xl font-bold text-white">{{ stats?.discord_support_members }}+</h1>
 					<h5 class="text-lg lg:text-xl text-white font-medium">— Discord members</h5>
 				</div>
 			</div>
@@ -205,7 +205,7 @@
 </template>
 
 <script lang="ts" setup>
-const toast = useToast(60000);
+const toast = useToast(5_000);
 const config = useRuntimeConfig();
 const { data: stats } = await useFetch('/api/stats');
 
