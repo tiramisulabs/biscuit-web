56c5aaaaa54f226e4d3ffaf9a3e71498b90b6927
kaname-png <56084970+kaname-png@users.noreply.github.com>
2023-01-14 11:14:59 -0600

    perf: some ui tweaks

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,29 @@
 	<div class="dark:bg-gray-900 bg-white min-h-screen">
 		<AppNavbar />
 		<slot />
-		<AppFooter />
+		<footer
+			class="bg-gray-50 dark:bg-gray-800 w-full p-6 flex items-center justify-center flex-col space-y-5 rounded-t-3xl text-center"
+		>
+			<NuxtImg
+				src="/icon.svg"
+				alt="biscuitland icon"
+				class="w-16 h-16 hover:bg-blue-600/10 dark:hover:bg-blue-400/10 p-2 cursor-pointer transition duration-300 rounded-full"
+			/>
+			<h1 class="text-xl dark:text-white text-black font-semibold">
+				Made with ❤️ by
+				<NuxtLink :href="config.public.secretDiscordSupportUrl" target="_blank">
+					<span
+						class="text-blue-400 hover:bg-blue-600/10 dark:hover:bg-blue-400/10 rounded-lg pl-1 transition duration-300 mr-1"
+					>
+						@biscuitland
+					</span>
+				</NuxtLink>
+				team.
+			</h1>
+		</footer>
 	</div>
 </template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+</script>
